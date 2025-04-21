7b9bc91529a78fba730f25944c4f6865cbd65f21
Kamil Kubiczek <kamil.kubiczek7@gmail.com>
2025-04-21 10:46:40 +0200

    fix/feat: fixed bug and updated about me

--- components/about-me/QuestionsSection.vue
+++ components/about-me/QuestionsSection.vue
@@ -2,54 +2,53 @@
    <section class="mx-auto flex items-center flex-col justify-center gap-12 max-w-[540px]">
       <AboutMeAnswerContainer>
          <AboutMeQuestionHeading>Who are you?</AboutMeQuestionHeading>
-         <AboutMeAnswerParagraph
-            >I'm 18 years old Polish IT high school student. Web development is my passion and work. In 2023 I passed INF-02 exam
-            with 96% points.</AboutMeAnswerParagraph
-         >
+         <AboutMeAnswerParagraph>I'm 18 years old Polish IT high school student. Web development is my passion and work.
+            In 2023 I passed INF-02 exam
+            with 96% points, and in 2025 I passed INF-03 exam with 97% score, which gave me IT technician title.
+         </AboutMeAnswerParagraph>
       </AboutMeAnswerContainer>
 
       <AboutMeAnswerContainer>
          <AboutMeQuestionHeading>What skills do you have?</AboutMeQuestionHeading>
-         <AboutMeAnswerParagraph
-            >I'm mainly skilled in Vue and Nuxt ecosystem, React, REST APIs, TRPC, BaaS. I gained some knowledge about
-            DevOps.</AboutMeAnswerParagraph
-         >
+         <AboutMeAnswerParagraph>I'm mainly skilled in Vue and Nuxt ecosystem, React, REST APIs, TRPC, BaaS. I gained
+            some knowledge about
+            DevOps.</AboutMeAnswerParagraph>
 
-         <AboutMeAnswerParagraph
-            >I already worked with GitHub, Gitlab, ClickUp in Agile methodology and with similar tools. Also I gained some
-            experience in UI/UX design on Figma.</AboutMeAnswerParagraph
-         >
+         <AboutMeAnswerParagraph>I already worked with GitHub, Gitlab, ClickUp in Agile methodology and with similar
+            tools. Also I gained some
+            experience in UI/UX design on Figma.</AboutMeAnswerParagraph>
 
-         <AboutMeAnswerParagraph
-            >You can see more of my known technologies on my
-            <span class="relative"
-               >GitHub profile.<AboutMeShapeUnderlineLong class="absolute -bottom-4 left-0 w-full scale-125" /> </span
-         ></AboutMeAnswerParagraph>
+         <AboutMeAnswerParagraph>You can see more of my known technologies on my
+            <span class="relative">GitHub profile.
+               <AboutMeShapeUnderlineLong class="absolute -bottom-4 left-0 w-full scale-125" />
+            </span>
+         </AboutMeAnswerParagraph>
       </AboutMeAnswerContainer>
 
       <SocialIcons />
 
       <AboutMeAnswerContainer>
          <AboutMeQuestionHeading>What type of projects do you build?</AboutMeQuestionHeading>
-         <AboutMeAnswerParagraph
-            >I can create small to medium size websites and webapps by myself with full process, from planning and desing to code.
-            I also do web based mobile apps with technologies like Ionic. For example I can do for you: landing pages, portfolio,
-            blog, business website, order tracker etc. For bigger projects contact me for details. Together we can create
+         <AboutMeAnswerParagraph>I can create small to medium size websites and webapps by myself with full process,
+            from planning and desing to code.
+            I also do web based mobile apps with technologies like Ionic. For example I can do for you: landing pages,
+            portfolio,
+            blog, business website, order tracker etc. For bigger projects contact me for details. Together we can
+            create
             something amazing!
          </AboutMeAnswerParagraph>
-         <AboutMeAnswerParagraph
-            >My main commercial projects can be found on the main portfolio page. Visit my GitHub profile for public projects I've
-            done.</AboutMeAnswerParagraph
-         >
-         <AboutMeAnswerParagraph
-            >Additionally I'm constanly building small and medium-size size private webapps/mobile web apps with other
-            programmers. Contact me for details or showcase.</AboutMeAnswerParagraph
-         >
+         <AboutMeAnswerParagraph>My main commercial projects can be found on the main portfolio page. Visit my GitHub
+            profile for public projects I've
+            done.</AboutMeAnswerParagraph>
+         <AboutMeAnswerParagraph>Additionally I'm constanly building small and medium-size size private webapps/mobile
+            web apps with other
+            programmers. Contact me for details or showcase.</AboutMeAnswerParagraph>
       </AboutMeAnswerContainer>
 
       <AboutMeAnswerContainer>
          <AboutMeQuestionHeading>And your languages?</AboutMeQuestionHeading>
-         <AboutMeAnswerParagraph>My main language is Polish. I also speak and write English well.</AboutMeAnswerParagraph>
+         <AboutMeAnswerParagraph>My main language is Polish. I also speak and write English well.
+         </AboutMeAnswerParagraph>
       </AboutMeAnswerContainer>
    </section>
 </template>
